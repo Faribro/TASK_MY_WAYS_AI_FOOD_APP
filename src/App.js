0f66db56b276a714a39cfd6af04554bad2cf8f39
Farid Sayyed <faridsayyed1010@gmail.com>
2023-04-14 13:22:30 +0000

    .

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import CreateFood from "./Form";
 import FoodList from "./List";
 import "./index.css";
 
-
 function App() {
   const [page, setPage] = useState("create");
 
   return (
     <div className="App">
-      <nav>
+      <nav className="navigation">
         <button onClick={() => setPage("create")}>Create Food</button>
         <button onClick={() => setPage("list")}>Food List</button>
       </nav>
