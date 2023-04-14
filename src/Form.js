1689cb50f9d0490d10bc4a0a27c8c1fa05852912
Farid Sayyed <faridsayyed1010@gmail.com>
2023-04-14 12:11:36 +0000

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 export default function FoodForm() {
